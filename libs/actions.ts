e94c2d1bdc89622c6b0b1dd3b5ccc5a154319e48
ryo-ma <saka_ro@yahoo.co.jp>
2021-08-20 23:18:52 +0900

    Optimize the initializetion part

--- libs/actions.ts
+++ libs/actions.ts
@@ -1,9 +1,12 @@
-import { ensureDir, ensureFile } from "../deps.ts";
+import { Colors, ensureDir, ensureFile } from "../deps.ts";
 import {
   DEFAULT_DATAFILES_PATH,
   DEFAULT_DIM_FILE_PATH,
   DEFAULT_DIM_LOCK_FILE_PATH,
+  DIM_LOCK_VERSION,
 } from "./consts.ts";
+import { Downloader } from "./downloader.ts";
+import { DimJSON, DimLockJSON } from "./types.ts";
 
 interface Action {
 }
@@ -13,24 +16,49 @@ export class InitAction implements Action {
     ensureDir(DEFAULT_DATAFILES_PATH);
   }
   static async createDimJson() {
-    ensureFile(DEFAULT_DIM_FILE_PATH);
-    ensureFile(DEFAULT_DIM_LOCK_FILE_PATH);
+    const dimFilePromise = new Promise(async () => {
+      const dimData: DimJSON = { data: [] };
+      await ensureFile(DEFAULT_DIM_FILE_PATH);
+      Deno.writeTextFile(DEFAULT_DIM_FILE_PATH, JSON.stringify(dimData));
+    });
+    const dimLockFilePromise = new Promise(async () => {
+      const dimLockData: DimLockJSON = {
+        lockFileVersion: DIM_LOCK_VERSION,
+        data: [],
+      };
+      await ensureFile(DEFAULT_DIM_LOCK_FILE_PATH);
+      Deno.writeTextFile(
+        DEFAULT_DIM_LOCK_FILE_PATH,
+        JSON.stringify(dimLockData),
+      );
+    });
+    Promise.all([dimFilePromise, dimLockFilePromise]);
   }
   async execute(options: any) {
-    console.log(options);
-    await Promise.all([InitAction.createDataFiles(), InitAction.createDimJson()])
+    await Promise.all([
+      InitAction.createDataFiles(),
+      InitAction.createDimJson(),
+    ]);
+    console.log(Colors.green("Initialized the project for the dim."));
   }
 }
 
 export class InstallAction implements Action {
-  execute(options: any, url: string): void {
-    console.log(options, url);
+  async execute(options: any, url: string) {
+    await new Downloader().download(new URL(url));
+    console.log(Colors.green(`Installed ${url}`));
   }
 }
 
 export class UninstallAction implements Action {
   execute(options: any, name: string): void {
     console.log(options, name);
+  }
+}
+
+export class ListAction implements Action {
+  execute(options: any): void {
+    console.log(options);
   }
 }
 
--- libs/consts.ts
+++ libs/consts.ts
@@ -3,3 +3,4 @@ export const VERSION = "0.1";
 export const DEFAULT_DATAFILES_PATH = "./data_files";
 export const DEFAULT_DIM_FILE_PATH = "./dim.json";
 export const DEFAULT_DIM_LOCK_FILE_PATH = "./dim-lock.json";
+export const DIM_LOCK_VERSION = 1.0;
