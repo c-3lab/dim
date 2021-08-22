61c1770d251f64fd9a196a19ebb6606f38c82f5a
ryo-ma <saka_ro@yahoo.co.jp>
2021-08-22 23:23:52 +0900

    Add unneccessary file to .gitignore

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export { download } from "https://deno.land/x/download/mod.ts";
-export type { DownlodedFile } from "https://deno.land/x/download/mod.ts";
+export { download } from "https://deno.land/x/download@v1.0.1/mod.ts";
+export type { DownlodedFile } from "https://deno.land/x/download@v1.0.1/mod.ts";
 export { Command } from "https://deno.land/x/cliffy@v0.19.5/command/mod.ts";
 export {
   DenoLandProvider,
@@ -16,7 +16,7 @@ export {
   existsSync,
 } from "https://deno.land/std@0.105.0/fs/mod.ts";
 
-export * as Colors from "https://deno.land/std/fmt/colors.ts";
+export * as Colors from "https://deno.land/std@0.105.0/fmt/colors.ts";
 
 export * as encoding from "https://cdn.skypack.dev/encoding-japanese";
 
