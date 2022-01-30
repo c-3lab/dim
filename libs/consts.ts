b3092d7d4a9208437c60cae05de43a747503fbea
ryo-ma <saka_ro@yahoo.co.jp>
2022-01-30 21:27:18 +0900

    Update to v0.1.5

--- libs/consts.ts
+++ libs/consts.ts
@@ -1,5 +1,5 @@
 export const NAME = "dim";
-export const VERSION = "v0.1.4";
+export const VERSION = "v0.1.5";
 export const DEFAULT_DATAFILES_PATH = "./data_files";
 export const DEFAULT_DIM_FILE_PATH = "./dim.json";
 export const DEFAULT_DIM_LOCK_FILE_PATH = "./dim-lock.json";
