741a8e8d7bb212da22010257fa0493960f9199f5
ta-Hirose <t-hirose@abelsoft.co.jp>
2022-08-17 14:38:17 +0900

    fix commit

--- libs/postprocess/xlsx_converter.ts
+++ libs/postprocess/xlsx_converter.ts
@@ -4,13 +4,6 @@ export class XLSXConverter {
     const workbook = await readXLSX(targetPath);
     const sheetData = workbook.Sheets[workbook.SheetNames[0]];
     const csv = xlsx.utils.sheet_to_csv(sheetData);
-    const splitPath = targetPath.split("/");
-    const dir = splitPath.slice(0, -1).join("/");
-    const fileName = splitPath[splitPath.length - 1].replace(
-      /\..xlsx$/,
-      ".csv",
-    );
-    const filePath = dir + "/" + fileName;
-    Deno.writeTextFileSync(filePath, csv);
+    Deno.writeTextFileSync(targetPath.replace(/\.xlsx$/, ".csv"), csv);
   }
 }
