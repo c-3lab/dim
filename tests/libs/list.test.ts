ca43e9c69b95ecdadfe97d8563a8b38b03716c57
ta-Hirose <t-hirose@abelsoft.co.jp>
2022-08-26 20:41:39 +0900

    Fix ListAction test

--- tests/libs/list.test.ts
+++ tests/libs/list.test.ts
@@ -17,6 +17,9 @@ import {
 
 import { ListAction } from "../../libs/actions.ts";
 import { removeTemporaryFiles, temporaryDirectory } from "../helper.ts";
+import { DimLockJSON } from "../../libs/types.ts";
+import { DEFAULT_DIM_LOCK_FILE_PATH } from "../../libs/consts.ts";
+import { Colors } from "../../deps.ts";
 
 const createEmptyDimJson = () => {
   Deno.writeTextFileSync(
@@ -56,11 +59,137 @@ describe("ListAction", () => {
 
   it("molded and displayed information on downloaded data", async () => {
     createEmptyDimJson();
+    const dimLockData: DimLockJSON = {
+      lockFileVersion: "1.1",
+      contents: [{
+        catalogResourceId: null,
+        catalogUrl: null,
+        eTag: null,
+        headers: {},
+        integrity: "",
+        lastDownloaded: new Date(),
+        lastModified: null,
+        name: "test1",
+        path: "./data_files/test1/dummy.txt",
+        postProcesses: ["encoding-utf-8"],
+        url: "https://example.com/dummy.txt",
+      }],
+    };
+    await Deno.writeTextFile(
+      DEFAULT_DIM_LOCK_FILE_PATH,
+      JSON.stringify(dimLockData, null, 2),
+    );
     await new ListAction().execute({});
+
+    assertSpyCall(consoleLogStub, 0, {
+      args: ["test1"],
+    });
+    assertSpyCall(consoleLogStub, 1, {
+      args: [
+        "  - URL               :",
+        Colors.green("https://example.com/dummy.txt"),
+      ],
+    });
+    assertSpyCall(consoleLogStub, 2, {
+      args: [
+        "  - Name              :",
+        Colors.green("test1"),
+      ],
+    });
+    assertSpyCall(consoleLogStub, 3, {
+      args: [
+        "  - File path         :",
+        Colors.green("./data_files/test1/dummy.txt"),
+      ],
+    });
+    assertSpyCall(consoleLogStub, 4, {
+      args: [
+        "  - Catalog URL       :",
+        Colors.green("null"),
+      ],
+    });
+    assertSpyCall(consoleLogStub, 5, {
+      args: [
+        "  - Catalog resourceid:",
+        Colors.green("null"),
+      ],
+    });
+    assertSpyCall(consoleLogStub, 6, {
+      args: [
+        "  - Last modified     :",
+        Colors.green("null"),
+      ],
+    });
+    assertSpyCall(consoleLogStub, 7, {
+      args: [
+        "  - ETag              :",
+        Colors.green("null"),
+      ],
+    });
+    assertSpyCall(consoleLogStub, 8, {
+      args: [
+        "  - Last downloaded   :",
+        Colors.green("2022-01-02T03:04:05.678Z"),
+      ],
+    });
+    assertSpyCall(consoleLogStub, 9, {
+      args: [
+        "  - Integrity         :",
+        Colors.green(""),
+      ],
+    });
+    assertSpyCall(consoleLogStub, 10, {
+      args: [
+        "  - Post processes    :",
+        Colors.green("encoding-utf-8"),
+      ],
+    });
+    assertSpyCall(consoleLogStub, 11, {
+      args: [
+        "  - Headers           :",
+        Colors.green("{}"),
+      ],
+    });
   });
 
   it("displays information on downloaded data without molding", async () => {
     createEmptyDimJson();
+    const dimLockData: DimLockJSON = {
+      lockFileVersion: "1.1",
+      contents: [{
+        catalogResourceId: null,
+        catalogUrl: null,
+        eTag: null,
+        headers: {},
+        integrity: "",
+        lastDownloaded: new Date(),
+        lastModified: null,
+        name: "test1",
+        path: "./data_files/test1/dummy.txt",
+        postProcesses: ["encoding-utf-8"],
+        url: "https://example.com/dummy.txt",
+      }],
+    };
+    await Deno.writeTextFile(
+      DEFAULT_DIM_LOCK_FILE_PATH,
+      JSON.stringify(dimLockData, null, 2),
+    );
     await new ListAction().execute({ simple: true });
+
+    assertSpyCall(consoleLogStub, 0, {
+      args: [
+        "test1",
+        "https://example.com/dummy.txt",
+        "./data_files/test1/dummy.txt",
+        null,
+        null,
+        null,
+        null,
+        "2022-01-02T03:04:05.678Z",
+        "",
+        "encoding-utf-8",
+        {},
+      ],
+    });
   });
 });
