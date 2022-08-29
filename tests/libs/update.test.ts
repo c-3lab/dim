2a8efa35def4241c115a35b66ace583e814aa63e
ta-Hirose <t-hirose@abelsoft.co.jp>
2022-08-29 14:31:49 +0900

    Fix updateAction test

--- tests/libs/update.test.ts
+++ tests/libs/update.test.ts
@@ -15,9 +15,14 @@ import {
   it,
 } from "https://deno.land/std@0.152.0/testing/bdd.ts";
 import { Colors } from "../../deps.ts";
-import { InstallAction, ListAction, UpdateAction } from "../../libs/actions.ts";
-import { DEFAULT_DIM_FILE_PATH, DIM_FILE_VERSION } from "../../libs/consts.ts";
-import { DimJSON } from "../../libs/types.ts";
+import { UpdateAction } from "../../libs/actions.ts";
+import {
+  DEFAULT_DIM_FILE_PATH,
+  DEFAULT_DIM_LOCK_FILE_PATH,
+  DIM_FILE_VERSION,
+  DIM_LOCK_FILE_VERSION,
+} from "../../libs/consts.ts";
+import { DimJSON, DimLockJSON } from "../../libs/types.ts";
 import {
   createKyGetStub,
   removeTemporaryFiles,
@@ -36,16 +41,16 @@ function fileExists(filePath: string): boolean {
 
 const createEmptyDimJson = () => {
   Deno.writeTextFileSync(
-    "dim.json",
-    JSON.stringify({ fileVersion: "1.1", contents: [] }),
+    DEFAULT_DIM_FILE_PATH,
+    JSON.stringify({ fileVersion: DIM_FILE_VERSION, contents: [] }),
   );
   Deno.writeTextFileSync(
-    "dim-lock.json",
-    JSON.stringify({ lockfileVersion: "1.1", contents: [] }),
+    DEFAULT_DIM_LOCK_FILE_PATH,
+    JSON.stringify({ lockfileVersion: DIM_LOCK_FILE_VERSION, contents: [] }),
   );
 };
 
-describe("UninstallAction", () => {
+describe("UpdateAction", () => {
   let consoleLogStub: Stub;
   let consoleErrorStub: Stub;
   let denoExitStub: Stub;
@@ -70,24 +75,324 @@ describe("UninstallAction", () => {
     consoleLogStub.restore();
   });
 
-  describe("with URL", () => {
-    it('execute with the "name" listed in dim.json to check if the data has been updated.', async () => {
-      createEmptyDimJson();
-      await new UpdateAction().execute({}, "example");
+  describe("with name", () => {
+    it('run update with "name" listed in dim.json', async () => {
+      const dimData: DimJSON = {
+        fileVersion: DIM_FILE_VERSION,
+        contents: [
+          {
+            name: "example",
+            url: "dummy",
+            catalogUrl: null,
+            catalogResourceId: null,
+            postProcesses: [],
+            headers: {},
+          },
+        ],
+      };
+      const dimLockData: DimLockJSON = {
+        lockFileVersion: DIM_LOCK_FILE_VERSION,
+        contents: [{
+          catalogResourceId: null,
+          catalogUrl: null,
+          eTag: null,
+          headers: {},
+          integrity: "",
+          lastDownloaded: new Date("2022-01-02T03:04:05.678Z"),
+          lastModified: null,
+          name: "example",
+          path: "./data_files/example/dummy.csv",
+          postProcesses: [],
+          url: "https://example.com/dummy.csv",
+        }],
+      };
+      await Deno.writeTextFile(
+        DEFAULT_DIM_FILE_PATH,
+        JSON.stringify(dimData, null, 2),
+      );
+      await Deno.writeTextFile(
+        DEFAULT_DIM_LOCK_FILE_PATH,
+        JSON.stringify(dimLockData, null, 2),
+      );
+      //  上書きされる前のファイルを用意
+      Deno.mkdirSync("data_files/example", { recursive: true });
+      Deno.writeTextFileSync("data_files/example/dummy.csv", "before");
+
+      const kyGetStub = createKyGetStub("after", {
+        headers: {
+          "etag": '"12345-1234567890abc"',
+          "last-modified": "Thu, 3 Feb 2022 04:05:06 GMT",
+        },
+      });
+      try {
+        //  重複する名前でintall
+        await new UpdateAction().execute({}, "example");
+
+        //  ファイルが更新されているか確認
+        const fileContent = Deno.readTextFileSync(
+          "data_files/example/dummy.csv",
+        );
+        assertEquals(fileContent, "after");
+
+        const dimJson = JSON.parse(
+          Deno.readTextFileSync(DEFAULT_DIM_FILE_PATH),
+        );
+        assertEquals(dimJson, {
+          fileVersion: DIM_FILE_VERSION,
+          contents: [{
+            catalogResourceId: null,
+            catalogUrl: null,
+            headers: {},
+            name: "example",
+            postProcesses: [],
+            url: "https://example.com/dummy.csv",
+          }],
+        });
+
+        const dimLockJson = JSON.parse(
+          Deno.readTextFileSync(DEFAULT_DIM_LOCK_FILE_PATH),
+        );
+        assertEquals(dimLockJson, {
+          lockFileVersion: DIM_LOCK_FILE_VERSION,
+          contents: [{
+            catalogResourceId: null,
+            catalogUrl: null,
+            eTag: "12345-1234567890abc",
+            headers: {},
+            integrity: "",
+            lastDownloaded: "2022-01-02T03:04:05.678Z",
+            lastModified: "2022-02-03T04:05:06.000Z",
+            name: "example",
+            path: "./data_files/example/dummy.csv",
+            postProcesses: [],
+            url: "https://example.com/dummy.csv",
+          }],
+        });
+
+        assertSpyCall(consoleLogStub, 0, {
+          args: [
+            Colors.green(
+              "Updated example.",
+            ),
+            "\nFile path:",
+            Colors.yellow(
+              "./data_files/example/dummy.csv",
+            ),
+          ],
+        });
+      } finally {
+        kyGetStub.restore();
+      }
     });
-    it('exit with error when run with "name" not listed in dim.json', async () => {
+    it('exit with error when run with "name" not listed in dim-lock.json', async () => {
+      denoExitStub.restore;
       createEmptyDimJson();
-      await new UpdateAction().execute({}, "example2");
+      await new UpdateAction().execute({}, "example2").catch(() => {
+        assertSpyCall(consoleErrorStub, 0, {
+          args: [
+            Colors.red(
+              "Faild to update. Not Found a content in the dim-lock.json. ",
+            ),
+          ],
+        });
+      });
+      // assertSpyCall(denoExitStub, 0, { args: [1] });
     });
   });
-  describe("", () => {
-    it("execute without specifying any options or arguments, and check if the data in dim.json has been updated.", async () => {
-      createEmptyDimJson();
-      await new UpdateAction().execute({ asyncInstall: true }, undefined);
+  describe("without name", () => {
+    it("update all data recorded in dim-lock.json.", async () => {
+      const kyGetStub = createKyGetStub("after", {
+        headers: {
+          "etag": '"12345-1234567890abc"',
+          "last-modified": "Thu, 3 Feb 2022 04:05:06 GMT",
+        },
+      });
+      try {
+        Deno.mkdirSync("data_files/test1", { recursive: true });
+        Deno.writeTextFileSync("data_files/test1/dummy.txt", "before");
+        const dimData: DimJSON = {
+          "fileVersion": DIM_FILE_VERSION,
+          "contents": [
+            {
+              name: "test1",
+              url: "https://example.com/dummy.txt",
+              catalogUrl: null,
+              catalogResourceId: null,
+              postProcesses: [
+                "encoding-utf-8",
+              ],
+              headers: {},
+            },
+            {
+              name: "test2",
+              url: "https://example.com/dummy.csv",
+              catalogUrl: null,
+              catalogResourceId: null,
+              postProcesses: [],
+              headers: {},
+            },
+            {
+              url: "https://example.com/dummy.zip",
+              name: "test3",
+              catalogUrl: null,
+              catalogResourceId: null,
+              postProcesses: [],
+              headers: {},
+            },
+          ],
+        };
+        await Deno.writeTextFile(
+          DEFAULT_DIM_FILE_PATH,
+          JSON.stringify(dimData, null, 2),
+        );
+        await new UpdateAction().execute({}, undefined);
+        const dimLockJson = JSON.parse(
+          Deno.readTextFileSync(DEFAULT_DIM_LOCK_FILE_PATH),
+        );
+        assertEquals(dimLockJson, {
+          lockFileVersion: DIM_LOCK_FILE_VERSION,
+          contents: [{
+            catalogResourceId: null,
+            catalogUrl: null,
+            eTag: "12345-1234567890abc",
+            headers: {},
+            integrity: "",
+            lastDownloaded: "2022-01-02T03:04:05.678Z",
+            lastModified: "2022-02-03T04:05:06.000Z",
+            name: "test1",
+            path: "./data_files/test1/dummy.txt",
+            postProcesses: ["encoding-utf-8"],
+            url: "https://example.com/dummy.txt",
+          }, {
+            catalogResourceId: null,
+            catalogUrl: null,
+            eTag: "12345-1234567890abc",
+            headers: {},
+            integrity: "",
+            lastDownloaded: "2022-01-02T03:04:05.678Z",
+            lastModified: "2022-02-03T04:05:06.000Z",
+            name: "test2",
+            path: "./data_files/test2/dummy.csv",
+            postProcesses: [],
+            url: "https://example.com/dummy.csv",
+          }, {
+            catalogResourceId: null,
+            catalogUrl: null,
+            eTag: "12345-1234567890abc",
+            headers: {},
+            integrity: "",
+            lastDownloaded: "2022-01-02T03:04:05.678Z",
+            lastModified: "2022-02-03T04:05:06.000Z",
+            name: "test3",
+            path: "./data_files/test3/dummy.zip",
+            postProcesses: [],
+            url: "https://example.com/dummy.zip",
+          }],
+        });
+        const fileContent = Deno.readTextFileSync(
+          "data_files/test1/dummy.txt",
+        );
+        assertEquals(fileContent, "after");
+      } finally {
+        kyGetStub.restore();
+      }
     });
-    it("specify -A and check for asynchronous execution", async () => {
-      createEmptyDimJson();
-      await new UpdateAction().execute({}, undefined);
+    it("asynchronous updating of all data recorded in dim-lock.json.", async () => {
+      const kyGetStub = createKyGetStub("after", {
+        headers: {
+          "etag": '"12345-1234567890abc"',
+          "last-modified": "Thu, 3 Feb 2022 04:05:06 GMT",
+        },
+      });
+      try {
+        Deno.mkdirSync("data_files/test1", { recursive: true });
+        Deno.writeTextFileSync("data_files/test1/dummy.txt", "before");
+        const dimData: DimJSON = {
+          "fileVersion": DIM_FILE_VERSION,
+          "contents": [
+            {
+              name: "test1",
+              url: "https://example.com/dummy.txt",
+              catalogUrl: null,
+              catalogResourceId: null,
+              postProcesses: [
+                "encoding-utf-8",
+              ],
+              headers: {},
+            },
+            {
+              name: "test2",
+              url: "https://example.com/dummy.csv",
+              catalogUrl: null,
+              catalogResourceId: null,
+              postProcesses: [],
+              headers: {},
+            },
+            {
+              url: "https://example.com/dummy.zip",
+              name: "test3",
+              catalogUrl: null,
+              catalogResourceId: null,
+              postProcesses: [],
+              headers: {},
+            },
+          ],
+        };
+        await Deno.writeTextFile(
+          DEFAULT_DIM_FILE_PATH,
+          JSON.stringify(dimData, null, 2),
+        );
+        await new UpdateAction().execute({ asyncInstall: true }, undefined);
+        const dimLockJson = JSON.parse(
+          Deno.readTextFileSync(DEFAULT_DIM_LOCK_FILE_PATH),
+        );
+        assertEquals(dimLockJson, {
+          lockFileVersion: DIM_LOCK_FILE_VERSION,
+          contents: [{
+            catalogResourceId: null,
+            catalogUrl: null,
+            eTag: "12345-1234567890abc",
+            headers: {},
+            integrity: "",
+            lastDownloaded: "2022-01-02T03:04:05.678Z",
+            lastModified: "2022-02-03T04:05:06.000Z",
+            name: "test1",
+            path: "./data_files/test1/dummy.txt",
+            postProcesses: ["encoding-utf-8"],
+            url: "https://example.com/dummy.txt",
+          }, {
+            catalogResourceId: null,
+            catalogUrl: null,
+            eTag: "12345-1234567890abc",
+            headers: {},
+            integrity: "",
+            lastDownloaded: "2022-01-02T03:04:05.678Z",
+            lastModified: "2022-02-03T04:05:06.000Z",
+            name: "test2",
+            path: "./data_files/test2/dummy.csv",
+            postProcesses: [],
+            url: "https://example.com/dummy.csv",
+          }, {
+            catalogResourceId: null,
+            catalogUrl: null,
+            eTag: "12345-1234567890abc",
+            headers: {},
+            integrity: "",
+            lastDownloaded: "2022-01-02T03:04:05.678Z",
+            lastModified: "2022-02-03T04:05:06.000Z",
+            name: "test3",
+            path: "./data_files/test3/dummy.zip",
+            postProcesses: [],
+            url: "https://example.com/dummy.zip",
+          }],
+        });
+        const fileContent = Deno.readTextFileSync(
+          "data_files/test1/dummy.txt",
+        );
+        assertEquals(fileContent, "after");
+      } finally {
+        kyGetStub.restore();
+      }
     });
   });
 });
