948273e07cbf22aa544bf0292bd8525841835b55
ryo-ma <saka_ro@yahoo.co.jp>
2022-10-19 11:01:34 +0900

    Refactor code of console_animation.ts

--- libs/console_animation.ts
+++ libs/console_animation.ts
@@ -2,27 +2,28 @@ import { Colors } from "../deps.ts";
 
 export class ConsoleAnimation {
   private intervalId!: number;
+  private index = 0;
+
   constructor(private animationPattern: string[], private message: string) {
   }
-  async start(delay: number) {
-    let index = 0;
-    await new Promise<number>(() => {
-      this.intervalId = setInterval(() => {
-        Deno.stdout.write(
-          new TextEncoder().encode(
-            Colors.brightBlue(
-              `\r${this.animationPattern[index++]}`,
-            ),
-          ),
-        );
-        Deno.stdout.write(
-          new TextEncoder().encode(
-            ` ${this.message}`,
-          ),
-        );
-        index %= this.animationPattern.length;
-      }, delay);
-    });
+  print = () => {
+    Deno.stdout.write(
+      new TextEncoder().encode(
+        Colors.brightBlue(
+          `\r${this.animationPattern[this.index++]}`,
+        ),
+      ),
+    );
+    Deno.stdout.write(
+      new TextEncoder().encode(
+        ` ${this.message}`,
+      ),
+    );
+    this.index %= this.animationPattern.length;
+  };
+  start(delay: number) {
+    this.index = 0;
+    this.intervalId = setInterval(this.print, delay);
   }
   stop() {
     if (this.intervalId !== undefined) {
